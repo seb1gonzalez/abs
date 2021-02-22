7d7580e9339645d9c6d10189ccafd68251ae7c52
javanikeed <javanikeed@gmail.com>
2021-02-21 21:27:36 -0500

    :pencil: Added main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+
+import logging
+from runner import Runner
+
+if __name__ == '__main__':
+    logging.getLogger().setLevel(logging.DEBUG)
+    logging.debug("Main(): Instantiated")
+    Runner()
---
+++ runner.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+
+import sys
+import logging
+import os
+
+
+class Runner:
+    def __init__(self):
+        logging.debug("Runner(): Instantiated")
+        sys.exit(0)
+
+
+
