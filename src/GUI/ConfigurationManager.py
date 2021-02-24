79a733ea7e49a2e432a250359c6d306d846ab905
Charlie Juarez <cjuare01@yahoo.com>
2021-02-24 14:37:29 -0700

    Fixed initial commit issue, Added config file, and added time range

--- src/GUI/ConfigurationManager.py
+++ src/GUI/ConfigurationManager.py
@@ -1,38 +1,76 @@
 import logging
+import json
 import sys
 
 from PyQt5 import QtGui
 from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QVBoxLayout, QHBoxLayout, QLabel
+from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QVBoxLayout, QHBoxLayout, QLabel, QSpinBox, QPushButton
 
 class ConfMng(QMainWindow):
     def __init__(self):
-        logging.info("ConfMng(): Instantiated")
+        logging.debug("ConfMng(): Instantiated")
         super(ConfMng, self).__init__()
         self.setWindowTitle("Agent Build System: Configuration Manager")
         mainwidget = QWidget()
         self.setCentralWidget(mainwidget)
         mainlayout = QVBoxLayout()
         layout1 = QHBoxLayout()
+        layout2 = QHBoxLayout()
+        layout3 = QHBoxLayout()
 
-        agent_name_label = QLabel("Name of agent:")
+        self.extract_json_data()
+
+        agent_name_label = QLabel("Name of Agent: " + self.data['Agent Name'])
         agent_name_label.setFont(QtGui.QFont("Times", weight = QtGui.QFont.Bold))
         agent_name_label.setAlignment(Qt.AlignLeft)
 
+        time_label = QLabel("Time Range:")
+        time_label.setFont(QtGui.QFont("Times", weight = QtGui.QFont.Bold))
+        time_label.setAlignment(Qt.AlignLeft)
+
+        self.timesp = QSpinBox()
+        self.timesp.setAlignment(Qt.AlignRight)
+        self.timesp.setMaximum(31540000) # Seconds in a year
+        self.timesp.setValue(self.data['Time Range'])
+
+        continue_butt = QPushButton("Continue")
+
+
         layout1.addWidget(agent_name_label)
 
+        layout2.addWidget(time_label)
+        layout2.addWidget(self.timesp)
+
+        layout3.addStretch()
+        layout3.addWidget(continue_butt)
+
+        mainlayout.addStretch()
         mainlayout.addLayout(layout1)
-        logging.info("ConfMng(): Complete")
+        mainlayout.addStretch()
+        mainlayout.addLayout(layout2)
+        mainlayout.addStretch()
+        mainlayout.addLayout(layout3)
+        mainlayout.addStretch()
+
+        mainwidget.setLayout(mainlayout)
+        logging.debug("ConfMng(): Complete")
+
+    def extract_json_data(self):
+        logging.debug("extract_json_data(): Instantiated")
+        json_file = open('/root/Practicum/abs/src/Config/Config.json')
+        self.data = json.load(json_file)
+        logging.debug(self.data)
+        json_file.close()
+        logging.debug("extract_json_data(): Complete")
+
 
 if __name__ == '__main__':
-    logging.info("main(): Instantiated")
-    logging.basicConfig(format='%(levelname)s:%(message)s', level = logging.DEBUG)
-    #appctxt = ApplicationContext()
+    #logging.getLogger().setLevel(logging.DEBUG)
+    logging.debug("main(): Instantiated")
+    logging.basicConfig(format='%(levelname)s:%(message)s')
     app = QApplication(sys.argv)
-    app = ConfMng()
-    #app.setGeometry(500, 300, 500, 150)
-    app.show()
-    #sys.exit(app.exec_())
+    ConfMngApp = ConfMng()
+    ConfMngApp.setGeometry(700, 450, 250, 300)
+    ConfMngApp.show()
     app.exec_()
-    #exit_code = appctxt.app.exec_()
-    logging.info("main(): Complete")
+    logging.debug("main(): Complete")
