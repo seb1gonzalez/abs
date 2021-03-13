bb14bec5ee17844bd037bf0b629cbcf5efaee719
Jesus Hurtado <you@example.com>
2021-03-13 13:29:01 -0700

    mqtree implemented delete hide

--- src/GUI/Builder/CasualRelationship.py
+++ src/GUI/Builder/CasualRelationship.py
@@ -3,7 +3,10 @@ from PyQt5.QtWidgets import QApplication, QLabel, QMainWindow, QMenuBar, QMenu, 
     QTextEdit, QPushButton, QVBoxLayout, QGridLayout, QTreeView
 import sys
 import json
-from src.GUI.JSONTREE.qjsonmodel import QJsonModel
+from JSONTREE.qjsonmodel import QJsonModel
+import queue
+
+ #= queue.Queue()
 
 class Color(QWidget):
 
@@ -17,11 +20,24 @@ class Color(QWidget):
 class CasualRelationship(QWidget):
     def __init__(self,treeView:QWidget):
         super(QWidget, self).__init__()
+        
+        
+
         self.layout = QGridLayout()
+
+        #self.hidden_items = queue.Queue()
+        #self.deleted_items = queue.Queue()
+
         self.delete_button = QPushButton("Delete")
-        self.hide_button = QPushButton("Hide")
         self.delete_button.setMaximumWidth(50)
+        self.delete_button.clicked.connect(self.clicked_delete)     
+        self.delete_button_check = 0   
+
+        self.hide_button = QPushButton("Hide")
         self.hide_button.setMaximumWidth(50)
+        self.hide_button.clicked.connect(self.clicked_hidden)
+        self.hide_button_check = 0
+
         self.layout.setColumnMinimumWidth(0,500)
         # self.layout.setColumnStretch(0,3)
         # self.layout.setColumnStretch(3,2)
@@ -33,6 +49,37 @@ class CasualRelationship(QWidget):
 
         self.setLayout(self.layout)
 
+    def clicked_delete(self):
+            if self.delete_button_check == 0:
+                self.delete_button_check = 1
+                self.setAutoFillBackground(True)
+                palette = self.palette()
+                palette.setColor(QPalette.Window, QColor("red"))
+                self.setPalette(palette)
+            elif self.delete_button_check == 1:
+                self.delete_button_check = 0
+                self.delete_button_check = 1
+                self.setAutoFillBackground(True)
+                palette = self.palette()
+                palette.setColor(QPalette.Window, QColor("white"))
+                self.setPalette(palette)
+        
+    def clicked_hidden(self):
+        if self.hide_button_check == 0:
+            self.hide_button_check = 1
+            self.setAutoFillBackground(True)
+            palette = self.palette()
+            palette.setColor(QPalette.Window, QColor("yellow"))
+            self.setPalette(palette)
+        else:
+            if self.hide_button_check == 1:
+                self.hide_button_check = 0
+                self.hide_button_check = 0
+                self.hide_button_check = 1
+                self.setAutoFillBackground(True)
+                palette = self.palette()
+                palette.setColor(QPalette.Window, QColor("white"))
+                self.setPalette(palette)
 
 def main():
     app = QApplication(sys.argv)
--- src/GUI/Builder/se.py
+++ src/GUI/Builder/se.py
@@ -1,23 +1,27 @@
 import json
-from src.GUI.JSONTREE.qjsonmodel import QJsonModel
+from JSONTREE.qjsonmodel import QJsonModel
 import traceback
 
 import sys
 
 from PyQt5.QtCore import Qt, QSize
 from PyQt5.QtWidgets import QApplication, QLabel, QMainWindow, QMenuBar, QMenu, QAction, QWidget, QFileDialog, \
-    QTextEdit, QPushButton, QVBoxLayout, QGridLayout, QTreeView, QListView, QListWidget, QScrollArea
+     QTextEdit, QPushButton, QVBoxLayout, QGridLayout, QTreeView, QListView, QListWidget, QScrollArea, QComboBox
 from PyQt5.QtGui import QPixmap, QStandardItemModel, QStandardItem
 from PyQt5.QtCore import QDir
-from src.GUI.Builder.CasualRelationship import CasualRelationship
-# from BuilderTreeView import TreeviewBuilder
+from Builder.CasualRelationship import CasualRelationship
+#from BuilderTreeView import TreeviewBuilder
+
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
 
 class DialogApp(QWidget):
     def __init__(self,parent):
         super().__init__(parent)
         try:
             # self.scroll = QScrollArea()
-            self.layout = QVBoxLayout()
+            #self.layout = QVBoxLayout()
 
             # # Scroll Area Properties
             # self.scroll.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOn)
@@ -25,12 +29,34 @@ class DialogApp(QWidget):
             # self.scroll.setWidgetResizable(True)
             # self.scroll.setWidget(self)
 
-            self.button1 = QPushButton('Import script')
-            self.button1.setMaximumWidth(80)
-            self.button1.setGeometry(0,0,80,20)
-            self.button1.clicked.connect(self.get_text_file)
-            self.layout.addWidget(self.button1)
-            self.setLayout(self.layout)
+            self.button_import = QPushButton("Import")
+            self.button_save = QPushButton("Save")
+            self.button_undo = QPushButton("Undo")
+
+            self.dropdown = QComboBox()
+            self.dropdown.addItems(["Network","Mouse-Clicks","Keystrokes"])
+            #self.dropdown.addItem("Network")
+            #self.dropdown.addItem("Mouse-Clicks")
+            #self.dropdown.addItem("Keystrokes")
+
+            self.hbox = QHBoxLayout()
+            self.vbox = QVBoxLayout()
+            
+            self.hbox.addWidget(self.button_import)
+            self.hbox.addWidget(self.button_save)
+            self.hbox.addWidget(self.button_undo)
+            self.hbox.addWidget(self.dropdown)
+            #self.setLayout(self.hbox)
+            
+            self.vbox.addLayout(self.hbox)
+            self.setLayout(self.vbox)
+
+            # self.button1 = QPushButton('Import script')
+            # self.button1.setMaximumWidth(80)
+            # self.button1.setGeometry(0,0,80,20)
+            self.button_import.clicked.connect(self.get_text_file)
+            # self.layout.addWidget(self.button1)
+            # self.setLayout(self.layout)
         except Exception:
             traceback.print_exc()
 
@@ -61,11 +87,11 @@ class DialogApp(QWidget):
 
                             casual_relationship = CasualRelationship(json_tree_view)
                             casual_relationship.setMinimumSize(1100,200)
-                            self.layout.addWidget(casual_relationship)
+                            self.vbox.addWidget(casual_relationship)
 
                             index += 1
                             if index == 5:
-                                break
+                                 break
                             print("Total items: ", index)
 
 
