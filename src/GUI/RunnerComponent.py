e4a98dc013206e8e5fea705d09b91ee620ce8f5f
javanikeed <javanikeed@gmail.com>
2021-03-28 23:37:44 -0400

    Modified runner functionality

--- src/GUI/RunnerComponent.py
+++ src/GUI/RunnerComponent.py
@@ -1,4 +1,5 @@
 from PyQt5.QtWidgets import (QApplication, QWidget, QInputDialog, QLineEdit, QLabel, QMessageBox, QFileDialog, QVBoxLayout, QGridLayout, QHBoxLayout, QPushButton, QPlainTextEdit)
+from PyQt5.QtGui import QTextCursor
 from Runner.Runner import Runner
 import sys
 import subprocess
@@ -23,12 +24,16 @@ class RunnerApp(QWidget):
         
 
         self.layout = QHBoxLayout()
-        self.button2 = QPushButton('Import Scripts')
+        self.button1 = QPushButton('Import Script')
+        self.button1.setFixedSize(125, 30)
+        self.layout = QHBoxLayout()
+        self.button2 = QPushButton('Clear Script')
         self.button2.setFixedSize(125, 30)
         
         self.scriptlabel = QLabel('Script: ')
-        self.scriptlabel.setWordWrap(True)
+        self.scriptlabel.setWordWrap(False)
         #self.scriptlabel.setFixedWidth(250)
+        self.layout.addWidget(self.button1)
         self.layout.addWidget(self.button2)
         self.layout.addWidget(self.scriptlabel)
 
@@ -53,24 +58,22 @@ class RunnerApp(QWidget):
         
         self.setLayout(self.mainlayout)
 
-        self.button2.clicked.connect(self.import_script_button_clicked)
+        self.button1.clicked.connect(self.import_script_button_clicked)
+        self.button2.clicked.connect(self.clear_loaded_script)
         self.button3.clicked.connect(self.run_agent_clicked)
         #self.show()
-        self.ECELd_clicked()
+        #self.ECELd_clicked()
 
 
     def ECELd_clicked(self):
-        alert = QMessageBox()
-        alert.setText('ECELd has been invoked!')
-        alert.exec()
+        self.display_alert('ECELd has been invoked!')
         Runner.run_eceld(self)
 
-
-
-    def set_script_name(self):
+    #helper fuction to set script name on gui
+    def set_script_name(self, cmd):
         self.scriptlabel.setText("Script: "+ cmd)
 
-
+    #import script
     def import_script_button_clicked(self):
         global cmd
         options = QFileDialog.Options()
@@ -78,24 +81,21 @@ class RunnerApp(QWidget):
         fileName, _ = QFileDialog.getOpenFileName(self, "QFileDialog.getOpenFileName()", "","All Files (*);;Python Files (*.py)", options=options)
         if fileName:
             print(fileName)
-        alert = QMessageBox()
-        alert.setText(fileName + " has been loaded.")
-        alert.exec()
+        self.display_alert(fileName + " has been loaded.")
         cmd = str(fileName)
         print(cmd)
-        self.set_script_name()
+        self.set_script_name(cmd)
         self.update_log_window("\nDEBUG:root:RunnerComponent.py(): Script Added.")
 
-
+    #run agent
     def run_agent_clicked(self):
         try:
             subprocess.run(cmd, shell=True)
-        except NameError:
-            alert = QMessageBox()
-            alert.setText('No script was specified.')
-            alert.exec()
-            print("No script was specified.")
+        except (NameError , TypeError):
+            self.display_alert('No script was specified.')
 
+
+    #initialize or grab preexisting log history
     def open_log_file(self):
         global content
         f = open('/home/kali/Desktop/abs/src/GUI/Runner/logs.txt', 'r')
@@ -103,12 +103,35 @@ class RunnerApp(QWidget):
         f.close()
         return content
 
+    #appends each log to log history
     def update_log_window(self, msg):
         f = open('/home/kali/Desktop/abs/src/GUI/Runner/logs.txt', 'a')
         f.write(msg)
         f.close()
         self.open_log_file()
         self.log.setPlainText(content)
+        self.log.moveCursor(QTextCursor.End)
+
+    #clears loaded script
+    def clear_loaded_script(self):
+        global cmd
+        try:
+            if cmd is None:
+                self.set_script_name('')
+                self.display_alert('Script has already been cleared.')
+            else:
+                cmd = None
+                self.set_script_name('')
+                self.update_log_window("\nDEBUG:root:RunnerComponent.py(): Script Removed.")
+        except (NameError, TypeError):
+            self.display_alert('Nothing to clear.')
+
+    #display message box alert
+    def display_alert(self, msg):
+        alert = QMessageBox()
+        alert.setText(msg)
+        alert.exec()
+        self.update_log_window("\nDEBUG:root:RunnerComponent.py(): "+msg)
 
 
     
