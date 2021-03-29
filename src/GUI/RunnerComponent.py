e5e2ec9a0b6e20d20fdf90c0989b5268feba9305
javanikeed <javanikeed@gmail.com>
2021-03-28 23:23:22 -0400

    Modified Runner

--- src/GUI/RunnerComponent.py
+++ src/GUI/RunnerComponent.py
@@ -21,19 +21,17 @@ class RunnerApp(QWidget):
         self.setWindowTitle(self.title)
         self.setGeometry(self.left, self.top, self.width, self.height)
         
-        self.layout = QVBoxLayout()
-        self.button = QPushButton('Invoke ECELd')
-        self.button.setFixedSize(250, 30)
+
+        self.layout = QHBoxLayout()
         self.button2 = QPushButton('Import Scripts')
-        self.button2.setFixedSize(250, 30)
+        self.button2.setFixedSize(125, 30)
         
         self.scriptlabel = QLabel('Script: ')
         self.scriptlabel.setWordWrap(True)
-        self.scriptlabel.setFixedWidth(250)
-        self.layout.addWidget(self.button)
+        #self.scriptlabel.setFixedWidth(250)
         self.layout.addWidget(self.button2)
         self.layout.addWidget(self.scriptlabel)
-        
+
         self.run = QHBoxLayout()
         self.run.addStretch(1)
         self.button3 = QPushButton('Run Agent')
@@ -42,29 +40,31 @@ class RunnerApp(QWidget):
 
         self.logwindow = QVBoxLayout()
         self.log = QPlainTextEdit()
-        self.log.setFixedWidth(300)
+        #self.log.setFixedWidth(300)
         self.log.setReadOnly(True)
         content = self.open_log_file()
         self.log.setPlainText(content)
         self.logwindow.addWidget(self.log)
         self.logwindow.addLayout(self.run)
         
-        self.mainlayout = QHBoxLayout()
+        self.mainlayout = QVBoxLayout()
         self.mainlayout.addLayout(self.layout)
         self.mainlayout.addLayout(self.logwindow)
         
         self.setLayout(self.mainlayout)
 
-        self.button.clicked.connect(self.ECELd_clicked)
         self.button2.clicked.connect(self.import_script_button_clicked)
         self.button3.clicked.connect(self.run_agent_clicked)
         #self.show()
+        self.ECELd_clicked()
+
 
     def ECELd_clicked(self):
         alert = QMessageBox()
         alert.setText('ECELd has been invoked!')
         alert.exec()
         Runner.run_eceld(self)
+
 
 
     def set_script_name(self):
