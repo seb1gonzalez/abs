6f9b9af551f8d918ac035b17b0c0d3f6b8bb7d9e
Charlie Juarez <cjuare01@yahoo.com>
2021-04-11 15:36:54 -0600

    Packager 2 of 2, export has been redone

--- components/packager/main_packager_window.py
+++ components/packager/main_packager_window.py
@@ -5,19 +5,19 @@ import logging
 #from PyQt5.QtGui import *
 from PyQt5.QtWidgets import QApplication, QTabWidget, QWidget, QVBoxLayout
 from Packager.ImportSubcomponent import ImportManager
-#from Packager.ExportSubcomponent import ExportManager
+from Packager.ExportSubcomponent import ExportManager
 
 class PackagerTabWindow(QTabWidget):
    def __init__(self, parent = None):
       super(PackagerTabWindow, self).__init__(parent)
       self.setGeometry(100, 100, 1000, 500)
       self.tab1 = QWidget()
-      #self.tab2 = QWidget()
+      self.tab2 = QWidget()
 		
       self.addTab(self.tab1,"Import")
-      #self.addTab(self.tab2,"Export")
+      self.addTab(self.tab2,"Export")
       self.import_UI()
-      #self.export_UI()
+      self.export_UI()
       self.setWindowTitle("Agent Build System: Packager")
 		
    def import_UI(self):
@@ -26,11 +26,11 @@ class PackagerTabWindow(QTabWidget):
        tab1_layout.addWidget(import_subcomp)
        self.tab1.setLayout(tab1_layout)
 		
-   """ def export_UI(self):
+   def export_UI(self):
        tab2_layout = QVBoxLayout()
-       builder = ExportManager(self)
-       tab2_layout.addWidget(builder)
-       self.tab2.setLayout(tab2_layout) """
+       export_subcomp = ExportManager(self)
+       tab2_layout.addWidget(export_subcomp)
+       self.tab2.setLayout(tab2_layout)
 		
 def main():
    app = QApplication(sys.argv)
